171cf250e59c3cc22503b9ca95b0b48ceb4cc7c7
Nik7676 <134999495+Nik7676@users.noreply.github.com>
2024-11-21 15:05:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_3sao5NjnN',
+        userPoolClientId: '7p23argg1e9u9j876d9ka4kofb',
+        region: 'ap-south-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
